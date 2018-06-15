ac027513f01acc6fb032751b90dcb49ef273cce9
piugomes <pedroaugustog.gp20@gmail.com>
2018-06-15 15:29:07 -0300

    Trabalho- Versão 1 (Beta)

---
+++ php/lista_hacker.php
@@ -0,0 +1,98 @@
+<?php
+	include "conexao.php";
+	include "funcao.php";
+	
+	valida_session();
+	
+?>
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+	<head>
+	
+		<meta charset="UTF-8" />
+		<title>Página de Listagem de Hackers</title>
+	
+	</head>
+	
+	<body>
+	
+		<?php 
+			menu();
+		?>
+		
+		<fieldset>
+			<for action="lista_hacker.php" method="post">
+				
+				<label>Filtrar por Codinome: </label>
+				<input type="text" name="filtroCodinome" />
+				<br />
+				<br />
+				
+				<label>Filtrar por paradeiro: </label>
+				<input type="text" name="filtroParadeiro" />
+				<br />
+				<br />
+				
+				<input type="submit" value="Enviar" />
+			
+			</for>
+			
+			<br />
+			
+			<!-- <form action="lista_hacker.php" method="post" name="ordenarHacker">
+				
+				<label>Ordenar</label>
+				
+				<select name="ordenacaoHacker" onchange="document.ordenarHacker.submit()">
+				
+					<option value="">:: Ordenar por ::</option>
+					
+					<option value="id_a_z">ID (Crescente)</option>
+					<option value="id_z_a">ID (Descrecente)</option>
+					
+					<option value="codinome_a_z">Codinome (A->Z)</option>
+					<option value="codinome_z_a">Codinome (Z->A)</option>
+					
+					<option value="paradeiro_a_z">Paradeiro (A->Z)</option>
+					<option value="paradeiro_z_a">Paradeiro (Z->A)</option>
+					
+				</select>
+				
+			</form>-->
+			
+		</fieldset>
+		
+		<table>
+			<thead>
+				<tr>
+					<th>Codinome Hacker</th>
+					<th>Paradeiro Operacional</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<?php 
+					$select = "SELECT * FROM hackers";
+					
+					$resultado = mysqli_query($link, $select) or die(mysqli_error($link));
+					
+					while($linha = mysqli_fetch_array($resultado)){
+					
+						echo "<tr>";
+							echo "<td>" . $linha['nome_hacker'] . "</td>";
+							echo "<td>" . $linha['paradeiro'] . "</td>";
+							
+							
+							
+							echo "<td> <a href='remove_hacker.php?id_hacker=" . $linha['id_hacker'] . "'>Excluir</a></td>";
+						echo "</tr>";
+					
+					}
+				?>
+			</tbody>
+		</table>
+	</body>
+
+</html>
